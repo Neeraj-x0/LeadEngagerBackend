e1af10ec731821d3d5ef75388dd7ad689bbb6803
Neeraj <contact@neerajx0.xyz>
2025-01-08 19:30:30 +0530

    make it better

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,45 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import { clerkMiddleware, createClerkClient } from "@clerk/express";
+import express, { Request, Response } from "express";
+import "dotenv/config";
 
-dotenv.config();
-
-const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+const app = express();
+
+const clerkClient = createClerkClient({
+  publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+  apiUrl: "https://api.clerk.com",
+  secretKey: process.env.CLERK_SECRET_KEY,
 });
 
+app.use(clerkMiddleware({ clerkClient }));
+
+app.use(clerkMiddleware());
+//@ts-ignore
+app.get("/auth-state", (req: any, res: any) => {
+  const authState = req.auth;
+  return res.json(authState);
+});
+
+app.get("/", async (req: Request, res: Response) => {
+  const { emailAddress, password } = req.query;
+
+  try {
+  const users = await clerkClient.users.getUserList({
+    emailAddress: [`${emailAddress}`],
+  });
+  const id = users.data[0].id;
+  const isPasswordVerified = await clerkClient.users.verifyPassword({
+    password: `${password}`,
+    userId: id,
+  });
+  let verifed = isPasswordVerified.verified;
+  res.json({ verifed });
+  }
+  catch (error:any) {
+    res.status(400).json({ error: error.message });
+  }
+});
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
